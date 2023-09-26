a3a096736debf174cdcd5f4a157dc220ba9dfbde
Shikharawat2002 <rawatsikha112@gmail.com>
2023-09-26 12:03:04 +0530

    finaladd

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function Github() {
+export default function Github()
+ {
+  const [data , setData] = useState([])
+
+  useEffect(()=>
+  {
+    fetch('https://api.github.com/users/shikharawat2002')
+    .then(response=>response.json())
+    .then(data=>{
+      setData[data];
+  })
+},[])
+
   return (
-    <div>Github</div>
+    <>
+    <div className='text-center m-4 bg-red-100 text-white p-4 text-3xl'>Github followers: {data.followers}
+    <img src={data.avatar_url} alt="Git picture" width={300} />
+    </div>
+    </>
   )
 }
 
-export default Github
